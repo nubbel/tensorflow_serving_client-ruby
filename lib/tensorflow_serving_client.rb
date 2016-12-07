4d2d11a861cc0ba5ec467fc9065c78319de4ce03
Dominique d’Argent <dominique.dargent@me.com>
2016-12-07 18:42:45 +0100

    Add import for example proto

--- lib/tensorflow_serving_client.rb
+++ lib/tensorflow_serving_client.rb
@@ -1,5 +1,6 @@
 require "tensorflow_serving_client/version"
 require 'tensorflow_serving/apis/prediction_service_services_pb'
+require 'tensorflow/core/example/example_pb'
 
 module TensorflowServingClient
   # Your code goes here...
